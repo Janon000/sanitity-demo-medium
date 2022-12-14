e72f5c75c9e7f4b0f5e9bedc91a2935a4245f275
Janon000 <brictoni@gmail.com>
2022-12-14 13:31:03 -0500

    Popup sign in modal

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,36 @@ import Link from 'next/link'
 import { signIn, signOut, useSession } from 'next-auth/react'
 
 function Header() {
-  const { data: session } = useSession()
+  const { data: session, status } = useSession()
+
+  const popupCenter = (url:string, title:string) => {
+    const dualScreenLeft = window.screenLeft ?? window.screenX;
+    const dualScreenTop = window.screenTop ?? window.screenY;
+
+    const width =
+      window.innerWidth ?? document.documentElement.clientWidth ?? screen.width;
+
+    const height =
+      window.innerHeight ??
+      document.documentElement.clientHeight ??
+      screen.height;
+
+    const systemZoom = width / window.screen.availWidth;
+
+    const left = (width - 500) / 2 / systemZoom + dualScreenLeft;
+    const top = (height - 550) / 2 / systemZoom + dualScreenTop;
+
+    const newWindow = window.open(
+      url,
+      title,
+      `width=${500 / systemZoom},height=${550 / systemZoom
+      },top=${top},left=${left}`
+    );
+
+    newWindow?.focus();
+    // console.log('test')
+  };
+
   return (
     <header
       className={`${
@@ -32,11 +61,11 @@ function Header() {
                   ? 'hidden'
                   : 'rounded-full border border-black bg-black px-4 py-1 text-sm text-white'
               }`}
-              onClick={!session ? () => signIn() : () => signOut()}
+              onClick={!session ? () => popupCenter("/google-signin", "Sample Sign In") : () => signOut()}
             >
               <p>{session ? `${session.user?.name}` : 'Sign In'}</p>
             </button>
-            <button onClick={!session ? () => signIn() : () => signOut()}>
+            <button onClick={!session ? () => popupCenter("/google-signin", "Sample Sign In") : () => signOut()}>
               <img
                 src={session?.user?.image || ''}
                 alt=""
